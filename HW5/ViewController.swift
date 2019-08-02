d04aea51cba25bfc5245eee76dd2c039b2adaf6e
Suzuki Mariko <>
2019-08-02 11:28:21 +0800

    initial commit

---
+++ HW5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW5
+//
+//  Created by Suzuki Mariko on 02/08/2019.
+//  Copyright © 2019 Mariko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'HW5' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for HW5
+	pod 'RevealingSplashView'
+	pod 'FontAwesome.swift'
+	pod 'IQKeyboardManagerSwift'
+end
